e4a870fc65f1db3094bcf5605393a9b5051e81bb
Emma Peatfield <epeatfield@pop-os.localdomain>
2021-01-27 20:07:36 -0800

    Adding github gist API to SearchPage, styling, installing necessary packages

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -1,5 +1,8 @@
 import { TextField, Button } from '@material-ui/core';
 import React, { Component } from 'react';
+import { Octokit } from '@octokit/rest';
+
+const gg = new Octokit();
 
 export default class SearchPage extends Component<any, any>{
     constructor(props) {
@@ -8,25 +11,34 @@ export default class SearchPage extends Component<any, any>{
             username: '',
             favorites: [],
             fetchedList: [],
+            showList: false
         }
     }
     handleChange(e) {
         this.setState({username: e.target.value})
     }
     handleClick(e) {
-        //Get Users
+        e.preventDefault();
+        this.getUsersGists();
+    }
+    getUsersGists() {
+        gg.request(`GET /users/${this.state.username}/gists`, {
+            username: this.state.username}).then((response: any) => {
+                const listUser = response.data.filter(o => o.owner.login === this.state.username);
+                this.setState({fetchedList: listUser, showList: true})
+            })
     }
     render() {
         return (
             <div>
-                <form>
+                <form className="Search-Bar">
                     <TextField 
                         id="outlined-basic" 
                         label="Enter User" 
                         variant="outlined" 
                         value={this.state.username} 
                         onChange={(e) => this.handleChange(e)} />
-                    <Button onClick={(e) => this.handleClick(e)}>Search</Button>
+                    <Button className="Submit-Button" variant="contained" onClick={(e) => this.handleClick(e)}>Search</Button>
                 </form>
             </div>
         )
