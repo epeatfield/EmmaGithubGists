f52bd816ee368d87b67b84e184f7d902b7201a01
Emma Peatfield <epeatfield@pop-os.localdomain>
2021-01-27 22:37:11 -0800

    Adding Table to Favorites Page with Styling & ability to remove favorites

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -1,9 +1,94 @@
-import React, { PureComponent } from 'react';
+import React, { Component, PureComponent } from 'react';
+import { 
+    Table, TableBody, TableCell, TableContainer, 
+    TableHead, TableRow, IconButton, Paper
+} from '@material-ui/core'
+import StarIcon from '@material-ui/icons/Star';
 
-export class FavoritesList extends PureComponent{
+const myStorage = window.localStorage;
+
+export class FavoritesList extends Component<any, any>{
+    constructor(props) {
+        super(props)
+        this.state = {
+            favorites: '',
+        }
+        this.removeFavorites = this.removeFavorites.bind(this)
+        this.fetchFavorites = this.fetchFavorites.bind(this)
+    }
+
+    saveFavorites(k?) {
+        if (k) {
+            myStorage.setItem('favorites', JSON.stringify(k));
+        } else {
+            myStorage.setItem('favorites', JSON.stringify(this.state.favorites));
+        }
+        this.fetchFavorites();
+    }
+
+    removeFavorites(listItem) {
+        let keys;
+        const currentStorage = this.state.favorites
+        if (currentStorage) {
+            keys = currentStorage
+            var removeIndex = keys.map(function (item) { return item.id; }).indexOf(listItem.id);
+            keys.splice(removeIndex, 1)
+            this.setState({ favorites: keys }, this.saveFavorites)
+        } else {
+            keys = [];
+            return
+        }
+    }
+
+    fetchFavorites() {
+        const f = myStorage.getItem('favorites');
+        let fave;
+        if (f) {
+            fave = JSON.parse(f)
+        } else {
+            fave = [];
+        }
+        this.setState({ favorites: fave });
+    }
+
+    componentDidMount() {
+        this.fetchFavorites();
+    }
+
     render() {
         return (
-            <div>This is my favorites pages</div>
-        )
+            <div className="Table">
+                <h1>Favorites</h1>
+                {this.state.favorites.length > 0 && (
+                    <TableContainer component={Paper}>
+                        <Table className={'table'} aria-label="simple table">
+                            <TableHead className={'Table-Header'}>
+                                <TableRow>
+                                    <TableCell>ID</TableCell>
+                                    <TableCell align="right">User</TableCell>
+                                    <TableCell align="right">URL</TableCell>
+                                    <TableCell align="right">Favorite</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.favorites.map((entry, index) => (
+                                    <TableRow key={entry.id}>
+                                        <TableCell component="th" scope="row">
+                                            {entry.id}
+                                        </TableCell>
+                                        <TableCell align="right"><a href={entry.url}>{entry.url}</a></TableCell>
+                                        <TableCell align="right">{entry.owner.login}</TableCell>
+                                        <TableCell align="right">
+                                            <IconButton onClick={() => this.removeFavorites(this.state.favorites[index])}>
+                                                <StarIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
+            </div>)
     }
 }
